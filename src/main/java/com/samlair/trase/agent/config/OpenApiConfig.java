d8744b4d811c9f334dff7e3a3b538d5ffbd73ff3
Samuel Lair <lair001@gmail.com>
2026-01-31 18:38:44 -0500

    Add OpenAPI metadata

---
+++ src/main/java/com/samlair/trase/agent/config/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package com.samlair.trase.agent.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * OpenAPI configuration for JWT security.
+ */
+@Configuration
+@OpenAPIDefinition(
+		info = @Info(title = "Trase Agents API", version = "v1"),
+		security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+		name = "bearerAuth",
+		type = SecuritySchemeType.HTTP,
+		scheme = "bearer",
+		bearerFormat = "JWT"
+)
+public class OpenApiConfig {
+}
